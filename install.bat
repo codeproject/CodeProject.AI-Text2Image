64306d7816230032010a94957b5b19e1ec61ad75
Chris Maunder <chris.maunder@codeproject.com>
2024-06-24 08:47:30 -0400

    Server 2.6.5 compatibility

--- install.bat
+++ install.bat
@@ -4,5 +4,8 @@
     @goto:eof
 )
 
+REM Backwards compatibility with Server 2.6.5
+if "!utilsScript!" == "" if "!sdkScriptsDirPath!" NEQ "" set utilsScript=%sdkScriptsDirPath%\utils.bat
+
 REM Download the YOLO models from the CodeProject models/ folder and store in /assets
 REM call "%utilsScript%" GetFromServer "models/" "models-yolo8-pt.zip" "assets" "Downloading Standard YOLO models..."

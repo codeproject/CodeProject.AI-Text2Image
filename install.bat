0169c3bb08d0772adbce46d86ae3a4c0f0467f37
Chris Maunder <chris.maunder@codeproject.com>
2024-06-29 09:55:35 -0400

    Updated install script comments

--- install.bat
+++ install.bat
@@ -1,3 +1,13 @@
+:: Installation script :::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::
+::                           Text2Image
+::
+:: This script is only called from ..\..\CodeProject.AI-Server\src\setup.bat in
+:: Dev setup, or ..\..\src\setup.bat in production
+::
+:: For help with install scripts, notes on variables and methods available, tips,
+:: and explanations, see /src/modules/install_script_help.md
+
 @if "%1" NEQ "install" (
     echo This script is only called from ..\..\CodeProject.AI-Server\src\setup.bat
     @pause
--- install.sh
+++ install.sh
@@ -1,3 +1,18 @@
+#!/bin/bash
+
+# Development mode setup script ::::::::::::::::::::::::::::::::::::::::::::::
+#
+#                            Text2Image
+#
+# This script is called from the SuperResolution directory using: 
+#
+#    bash ../../CodeProject.AI-Server/src/setup.sh
+#
+# The setup.sh script will find this install.sh file and execute it.
+#
+# For help with install scripts, notes on variables and methods available, tips,
+# and explanations, see /src/modules/install_script_help.md
+
 if [ "$1" != "install" ]; then
     read -t 3 -p "This script is only called from: bash ../../CodeProject.AI-Server/src/setup.sh"
     echo
